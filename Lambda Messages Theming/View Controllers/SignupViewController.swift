a67b9362b1dfb1228c8e5b46c818453d8c82980e
iyinraphael <iyinraphael@gmail.com>
2019-01-06 11:47:29 -0800

    set background of sign-up view and customised button Appearance

--- Lambda Messages Theming/View Controllers/SignupViewController.swift
+++ Lambda Messages Theming/View Controllers/SignupViewController.swift
@@ -12,7 +12,11 @@ class SignupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        styleSignupButton()
+        
+        view.backgroundColor = Appearance.backgroundGray
+        
         guard AuthenticationHelper.currentUser != nil else { return }
         
         performSegue(withIdentifier: "ViewMessageList", sender: nil)
@@ -24,6 +28,14 @@ class SignupViewController: UIViewController {
         AuthenticationHelper.setCurrentUser(to: username)
         
         performSegue(withIdentifier: "ViewMessageList", sender: nil)
+    }
+    
+    func styleSignupButton(){
+        signupButton.backgroundColor = Appearance.lambdaRed
+        let cornerRadius = max(signupButton.frame.width, signupButton.frame.height) / 8.0
+        signupButton.setTitle("Add", for: .normal)
+        signupButton.setTitleColor(.white, for: .normal)
+        signupButton.layer.cornerRadius = cornerRadius
     }
     
     @IBOutlet weak var signupButton: UIButton!
